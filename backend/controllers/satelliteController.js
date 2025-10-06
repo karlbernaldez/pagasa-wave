88bb2e63ef61dfbdf2c5a2cc4a151f1b454a17c6
Karl Santiago Bernaldez <bernaldezkarlsantiago@gmail.com>
2025-10-06 01:33:46 +0000

    refactor codebase

--- backend/controllers/satelliteController.js
+++ backend/controllers/satelliteController.js
@@ -1,9 +1,8 @@
 import fetch from "node-fetch";
 
-const TOKEN = "FmEXN3EfzxsgjvIFRvBfbqMqMUA6uhCcEEhKIVmL";
-const XSRF = 'eyJpdiI6ImFsQVU4RkMxNm9HQVpiMGpaYjg5RWc9PSIsInZhbHVlIjoiVlZ3V1V4QU9JeXhETmluY0x4MWIxWnhwenFNUWxMT2dERVJtdmJMSzVWM0FXOXB0bzB0aFFIWlEvcVg3b3lIK29aQWdqQ2RvS1NBNy8yOUJxUG5ZRDA3ZVlvR3ozMm84Vm5Hc1dEU3d5S2xaT1NiZ2NNY0lhN2t2MzZyalV5UVMiLCJtYWMiOiJhNDBhNGMwNmY3OTQ0OTlkNzg3OWMyZjBlZWM0MzI1NzBiNjQ2MTgyOTQ4ZjM3Mjc3MzJiNTI3OWI1M2Q1ZjI4IiwidGFnIjoiIn0';
-const panahon_session = 'eyJpdiI6IjB3N0NnOXZFb1hvRHdiM0R2WVlleVE9PSIsInZhbHVlIjoiL3IwbytJbDhlQ3F1NmhXeFFxTU4yZDRJbmEzSXNaakt5LzFESHRkT3c0Vllrb21sNXZOL2FyR01vNjk5Ui9vRStTdzdCaTk4SUVxZGZyL2JxcnBQeWh0c3hPRGZ4NlVYNWRwM0xzZytKMEMvdEUraURVYklDTWVGWlc0WWdBeFkiLCJtYWMiOiJjYjQ4MjkzMTc0ZDQ5MTdiYTkwYjBkMGNkM2E3ZmFmYmU1MDZmMWQ4NWYzM2ZkNmNlYWMyYzE3NDI0Y2I2OWU3IiwidGFnIjoiIn0'
-
+const TOKEN = process.env.TOKEN || "";
+const XSRF =  process.env.XSRF_TOKEN || "";
+const panahon_session = process.env.PANAHON_SESSION || "";
 // ⚠️ Replace with your actual cookies from panahon.gov.ph session
 const PANAHON_COOKIE = `_ga=GA1.1.895444753.1751268319; _ga_W4CK6LCPJQ=GS2.1.s1759456400$o27$g1$t1759456557$j57$l0$h0; panahon_session= ${panahon_session}; XSRF-TOKEN= ${XSRF}`;
 
