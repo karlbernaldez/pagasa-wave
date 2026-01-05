56d1f7133799e8ec2a1335234ac235f7d958a362
Karl Santiago Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-01-05 08:28:42 +0000

    fix: renamed Edit directory to Wavelab and smoothen the line creation

--- frontend/src/utils/mapUtils.js
+++ frontend/src/utils/mapUtils.js
@@ -1,9 +1,9 @@
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import drawStyles from '../components/Edit/draw/styles';
-import DrawLineString from '../components/Edit/draw/linestring';
-import DrawRectangle from '../components/Edit/draw/rectangle';
-import DrawCircle from '../components/Edit/draw/circle';
-import SimpleSelect from '../components/Edit/draw/simple_select';
+import drawStyles from '../components/WaveLab/draw/styles';
+import DrawLineString from '../components/WaveLab/draw/linestring';
+import DrawRectangle from '../components/WaveLab/draw/rectangle';
+import DrawCircle from '../components/WaveLab/draw/circle';
+import SimpleSelect from '../components/WaveLab/draw/simple_select';
 
 function applyWatermark(ctx, width, height, { text, style, font, color, opacity }) {
   ctx.save();
