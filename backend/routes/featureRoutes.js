79a77af72204a1eca48c54643f58b643e99e4aab
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-06-22 17:22:46 +0800

    Add annotation change request route

--- backend/routes/featureRoutes.js
+++ backend/routes/featureRoutes.js
@@ -1,19 +1,28 @@
 // backend/routes/featureRoutes.js
 import express from 'express';
-import { createFeature, getAllFeatures, getFeaturesByUserAndProject, getFeatureBySourceId, deleteFeature, updateFeatureName, getProjectFeatureCollection, updateFeatureCoordinates } from '../controllers/featureController.js';
+import {
+  createFeature,
+  getAllFeatures,
+  getFeaturesByUserAndProject,
+  getFeatureBySourceId,
+  deleteFeature,
+  updateFeatureName,
+  getProjectFeatureCollection,
+  updateFeatureCoordinates,
+  requestFeatureChange,
+} from '../controllers/featureController.js';
 import protect from '../middleware/authMiddleware.js';
 import { authenticateToken } from '../middleware/authenticateToken.js';
 import { isOwnerOrAdmin, isFeatureOwnerOrAdmin } from '../middleware/featuresMiddleware.js';
 
 const router = express.Router();
 
-// Protect all feature routes
 router.use(protect);
 
-// Routes
 router.post('/', authenticateToken, createFeature); //checked
 router.get('/admin/project/:projectId/features', isOwnerOrAdmin, getProjectFeatureCollection);
 router.get('/my-projects/:projectId', isOwnerOrAdmin, getFeaturesByUserAndProject); // checked
+router.post('/:sourceId/request-change', requestFeatureChange);
 router.get('/:sourceId', isFeatureOwnerOrAdmin, getFeatureBySourceId); // checked
 router.delete('/:sourceId', isFeatureOwnerOrAdmin, deleteFeature); // checked
 router.patch('/:sourceId/coordinates', isFeatureOwnerOrAdmin, updateFeatureCoordinates);
