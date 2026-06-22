894f6e594b0ca6c7d018d7f624ea72ea9794a46e
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-06-22 10:59:47 +0800

    Add legacy forecaster project library page

---
+++ frontend/src/dashboards/forecaster/pages/LegacyProjectLibraryPage.jsx
@@ -0,0 +1,5 @@
+import ProjectLibraryPage from '@/features/projects/ProjectLibraryPage';
+
+export default function LegacyProjectLibraryPage(props) {
+  return <ProjectLibraryPage role="forecaster" {...props} />;
+}
