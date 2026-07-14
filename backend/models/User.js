d546b8fd48dba8833908514370ee2f03b1925222
Karl Santiago Bernaldez <kakieiceangel548@gmail.com>
2026-07-14 14:46:56 +0800

    fix: added forecaster in user role model

--- backend/models/User.js
+++ backend/models/User.js
@@ -127,7 +127,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["user", "admin"],
+      enum: ["user", "forecaster","admin"],
       default: "user",
       index: true
     },
