aeeb44311109b9205202f44b4b7e15bbd72e00f6
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-08-04 14:31:19 +0800

    Format map instance registry tests

--- frontend/src/dashboards/forecaster/map/helpers/mapInstance.test.js
+++ frontend/src/dashboards/forecaster/map/helpers/mapInstance.test.js
@@ -1,10 +1,6 @@
 import { afterEach, describe, expect, it, vi } from 'vitest';
 
-import {
-  getLatestMapInstance,
-  registerMapInstance,
-  subscribeToMapInstance,
-} from './mapInstance';
+import { getLatestMapInstance, registerMapInstance, subscribeToMapInstance } from './mapInstance';
 
 describe('shared Mapbox instance registry', () => {
   afterEach(() => {
